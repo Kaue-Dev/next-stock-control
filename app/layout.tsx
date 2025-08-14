6d493b298ed0362eed2efcebdb5e49274bed8d9d
Kaue-Dev <kaueandradepadilha@gmail.com>
2025-08-14 13:59:40 -0300

    feat: add shadcn toaster

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { SidebarProvider } from "@/components/ui/sidebar";
 import { AppSidebar } from "@/components/custom/app-sidebar";
+import { Toaster } from "sonner";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -34,6 +35,7 @@ export default function RootLayout({
           <main className="w-full">
             {children}
           </main>
+          <Toaster />
         </SidebarProvider>
       </body>
     </html>
